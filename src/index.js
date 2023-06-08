c2f9eb0dd6f3ca022308d1abf8117c35e8f0c148
tonnytech <tonnytei4@gmail.com>
2023-06-08 17:27:12 +0300

    add single products section

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render( <ProductsProvider>
-    
+root.render( <ProductsProvider> 
     <App />
 </ProductsProvider>);
